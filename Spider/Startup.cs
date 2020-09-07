ef16ada9990c450dee1eb92e3873221af074cac5
arif hussain <arif@croem.net>
2020-09-07 19:42:47 +0500

    SpiderWeb project Added

---
+++ Spider/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Spider.Startup))]
+namespace Spider
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
